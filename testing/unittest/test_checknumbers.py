6f7f1746ecb57c92c2d12683af51d955f1c27a4b
silvareal <sylvernusakubo@yahoo.com>
2018-07-21 20:19:31 -0700

    test checknumbers

---
+++ testing/unittest/test_checknumbers.py
@@ -0,0 +1,18 @@
+'''
+ unittesting Inherit from the class `unittest
+'''
+import unittest
+
+
+class CheckNumbers(unittest.TestCase):
+
+    # This test has to be ok
+    def test_int_float(self):
+        self.assertEquals(1, 1.0)
+
+    # This test fails
+    def test_str_float(self):
+        self.assertEquals(1, "1")
+
+if __name__ == "__main__":
+    unittest.main()
